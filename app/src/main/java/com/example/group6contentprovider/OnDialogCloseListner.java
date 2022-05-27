289a9286bca6ff98c826fecc090d98f7f13e6e70
aljonardddd <aljonard.delacruz123@gmail.com>
2022-05-27 18:35:26 +0800

    G6 Content Provider

---
+++ app/src/main/java/com/example/group6contentprovider/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.example.group6contentprovider;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
